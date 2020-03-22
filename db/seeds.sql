dd5de2f81c72429a29022380c1d26286577976f8
Kauri Nanda <bknanda6@gmail.com>
2020-03-22 00:39:55 -0600

    pushing all files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Mushroom', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Chipotle', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sriracha Seitan', false);
