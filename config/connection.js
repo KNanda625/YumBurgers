a21b98776bca92082c09eefd0146914aa6da5631
Kauri Nanda <bknanda6@gmail.com>
2020-03-25 21:15:03 -0600

    css styling

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "fpg5fc58z7nlfp04",
-    password: "l1353qgsxvhyg4d7",
-    database: "br0niwsc7mmtevm5"
-  });
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+      host: "qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "fpg5fc58z7nlfp04",
+      password: "l1353qgsxvhyg4d7",
+      database: "br0niwsc7mmtevm5"
+      })
+};
+
+// var connection = mysql.createConnection({
+//     host: "qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "fpg5fc58z7nlfp04",
+//     password: "l1353qgsxvhyg4d7",
+//     database: "br0niwsc7mmtevm5"
+//   });
   
   // Make connection.
   connection.connect(function(err) {
